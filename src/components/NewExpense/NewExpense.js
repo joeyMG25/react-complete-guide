dd7d1579cf2f086e94f8333cd1ece5ff068e3034
joeyMG25 <60983676+joeyMG25@users.noreply.github.com>
2022-03-17 07:34:33 -0500

    Upating code 2\

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,22 +1,39 @@
-import react from "react";
+import React, { useState } from "react";
 
 import './NewExpense.css';
 
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) =>{
+    const [newExpenseButton, setNewExpenseButton] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) =>{
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
 
+        
+
         props.onAddExpense(expenseData);
+        setNewExpenseButton(false);
     };
+
+    const newExpenseBtnHandler = () => {
+        setNewExpenseButton(true);
+        console.log('The button is working');
+    }
+
+    const cancelButton = () => {
+        setNewExpenseButton(false);
+    }
+
+    
 
     return(
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!newExpenseButton && <button onClick={newExpenseBtnHandler}>Add New Expense</button>}
+            {newExpenseButton && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={cancelButton} />}
         </div>
     );
 
